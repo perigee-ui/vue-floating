18e3327301eb373a2358c97f26d1a86861e4b8eb
teleskop150750 <teleskop150750@gmail.com>
2024-07-29 12:24:07 +0300

    chore(playground):  add useTypeahead

---
+++ playground/vue-app/src/pages/use-typeahead.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Base from '@/components/UseTypeahead/Base.vue'
+</script>
+
+<template>
+  <div>
+    <h1 class="text-5xl font-bold underline mb-6">
+      <a href="https://floating-ui.com/docs/useTypeahead" target="_blank" rel="noopener noreferrer">useTypeahead</a>
+    </h1>
+
+    <div class="grid grid-cols-3 place-items-center border border-slate-400 rounded lg:w-[40rem] h-[20rem] mb-4">
+      <Base />
+    </div>
+  </div>
+</template>
